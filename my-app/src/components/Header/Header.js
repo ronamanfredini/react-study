e83e0faf8aa79b3a0ad439d13580e8c24a07e146
Ronaldo Manfredini <jmanfredini@hotmail.com>
2020-10-02 20:39:53 -0300

    initial

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div className="header">
+			<ul>
+				<li>App Gestão</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Header;
---
+++ my-app/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+	return (
+		<div className="">
+			
+		</div>
+	);
+};
+
+export default MainPage;
+
